21e2d335dd6d0d12418c13865b1c1b9a2265160f
INGCapaDev <ingcapadev@gmail.com>
2024-03-11 09:23:04 -0700

    :test_tube: [TEST](clusters): Add storybook component story

---
+++ src/clustering/cluster.stories.tsx
@@ -0,0 +1,71 @@
+import { Meta, StoryObj } from 'storybook-framework-qwik/*';
+import Cluster, { ClusterProps } from './cluster';
+import { cluster } from './clustering';
+
+const DATASET = [
+  {
+    lng: 0,
+    lat: 0,
+  },
+  {
+    lng: 0,
+    lat: 1,
+  },
+  {
+    lng: 1,
+    lat: 0,
+  },
+  {
+    lng: 1,
+    lat: 1,
+  },
+];
+
+const DATASET2 = [
+  {
+    lng: 0,
+    lat: 0,
+  },
+  {
+    lng: 1,
+    lat: 1,
+  },
+  {
+    lng: 2,
+    lat: 2,
+  },
+  {
+    lng: 3,
+    lat: 3,
+  },
+];
+
+export default {
+  title: 'Clustering/Cluster',
+  component: Cluster,
+} as Meta<ClusterProps>;
+
+type Story = StoryObj<ClusterProps>;
+
+const squareDataset = cluster(DATASET, 10, 2);
+const diagonalDataSet = cluster(DATASET2, 1, 1);
+
+export const Square: Story = {
+  args: {
+    dataset: squareDataset,
+    width: 250,
+    height: 250,
+    size: 20,
+  },
+  render: (props) => <Cluster {...props} />,
+};
+
+export const Diagonal: Story = {
+  args: {
+    dataset: diagonalDataSet,
+    width: 250,
+    height: 250,
+    size: 20,
+  },
+  render: (props) => <Cluster {...props} />,
+};
